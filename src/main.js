4cdadb9528a69e470d4be0b46068bfcfed169106
Ananthakrishnan Baji <57234359+yobaji@users.noreply.github.com>
2022-06-29 14:07:29 +0530

    fix: change client id

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import vue3GoogleLogin from "vue3-google-login";
 const app = createApp(App);
 app.use(vue3GoogleLogin, {
   clientId:
-    "352931034399-ht1i7mqefgrbsn67a4b1nm991bvat47l.apps.googleusercontent.com",
+    "YOUR_CLIENT_ID",
 });
 app.mount("#app");
